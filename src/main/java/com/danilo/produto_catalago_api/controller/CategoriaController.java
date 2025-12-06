87ccd8f2643d2c5f198d3b57c6f8819e9078fe2d
Danilo <99danilorobert@gmail.com>
2025-12-06 10:49:06 -0300

    implementando swagger

--- src/main/java/com/danilo/produto_catalago_api/controller/CategoriaController.java
+++ src/main/java/com/danilo/produto_catalago_api/controller/CategoriaController.java
@@ -2,7 +2,9 @@ package com.danilo.produto_catalago_api.controller;
 
 import com.danilo.produto_catalago_api.business.CategoriaService;
 import com.danilo.produto_catalago_api.business.dto.CategoriaDTO;
-import com.danilo.produto_catalago_api.infrastructure.entity.Categoria;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,27 +12,45 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/categoria")
+@Tag(name = "Categorias", description = "Gerenciamento de categorias")
 public class CategoriaController {
 
     private final CategoriaService categoriaService;
 
+
     @PostMapping
+    @Operation(summary = "Criar categoria", description = "Cria uma nova categoria")
+    @ApiResponse(responseCode = "200", description = "Categoria criada com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<CategoriaDTO> criarCategoria(@RequestBody CategoriaDTO dto){
         return ResponseEntity.ok(categoriaService.criar(dto));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar por ID", description = "Busca a categoria por ID")
+    @ApiResponse(responseCode = "200", description = "Categoria localizada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id){
         return ResponseEntity.ok(categoriaService.buscarPorId(id));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar Categoria", description = "Atualiza a categoria")
+    @ApiResponse(responseCode = "200", description = "Categoria atualizada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<CategoriaDTO> atualizar(@PathVariable Long id,
                                                   @RequestBody CategoriaDTO dto){
         return ResponseEntity.ok(categoriaService.atualizar(id, dto));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar Categoria", description = "Deleta a categoria")
+    @ApiResponse(responseCode = "200", description = "Categoria deletada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
         categoriaService.deletar(id);
         return ResponseEntity.ok().build();
--- src/main/java/com/danilo/produto_catalago_api/controller/ProdutoController.java
+++ src/main/java/com/danilo/produto_catalago_api/controller/ProdutoController.java
@@ -2,6 +2,9 @@ package com.danilo.produto_catalago_api.controller;
 
 import com.danilo.produto_catalago_api.business.ProdutoService;
 import com.danilo.produto_catalago_api.business.dto.ProdutoDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,27 +12,44 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/produto")
+@Tag(name = "Produtos", description = "Gerenciamento de produtos")
 public class ProdutoController {
 
     private final ProdutoService produtoService;
 
     @PostMapping
+    @Operation(summary = "Criar produto", description = "Cria um novo produto")
+    @ApiResponse(responseCode = "200", description = "Produto criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<ProdutoDTO> criarProduto(@RequestBody ProdutoDTO dto){
         return ResponseEntity.ok(produtoService.criar(dto));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar por ID", description = "Busca o produto por ID")
+    @ApiResponse(responseCode = "200", description = "Produto localizado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<ProdutoDTO> buscarPorId(@PathVariable Long id){
         return ResponseEntity.ok(produtoService.buscarPorId(id));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar Produto", description = "Atualiza o produto")
+    @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<ProdutoDTO> atualizar(@RequestBody ProdutoDTO dto,
                                                 @PathVariable Long id){
         return ResponseEntity.ok(produtoService.atualizar(dto, id));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar Produto", description = "Deleta o produto")
+    @ApiResponse(responseCode = "200", description = "Produto deletado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro de servidor")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
         produtoService.deletar(id);
         return ResponseEntity.ok().build();
