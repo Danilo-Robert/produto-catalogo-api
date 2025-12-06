c7c54ecba8d6d1bc0463b322d6a69e698943e8f3
Danilo <99danilorobert@gmail.com>
2025-12-05 21:59:10 -0300

    primeiro commit

---
+++ src/main/java/com/danilo/produto_catalago_api/ProdutoCatalagoApiApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.produto_catalago_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoCatalagoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoCatalagoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/produto_catalago_api/ProdutoCatalagoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.produto_catalago_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoCatalagoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
